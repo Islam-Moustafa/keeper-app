bbfa40b739ec3bdcf9c7b28f50e1c6dfa3ce937b
Your Name <you@example.com>
2021-02-20 19:37:28 +0200

    first commit

---
+++ src/notes.js
@@ -0,0 +1,19 @@
+const notes = [
+    {
+        key: 1,
+        title: "Delegation",
+        content: "How many Delegation"
+    },
+    {
+        key: 2,
+        title: "Loops",
+        content: "How many Loops"
+    },
+    {
+        key: 3,
+        title: "Arrays",
+        content: "How many Arrays"
+    }
+]
+
+export default notes;
